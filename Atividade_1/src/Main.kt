5d32682367c228177bc7b431c19d4fbb95cb584b
Eduardo Fassini <151883534+EduardoFassini@users.noreply.github.com>
2024-08-21 11:22:05 -0300

    Add files via upload

---
+++ Atividade_1/src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    var idade: Int
+    var altura: Double
+    var nome: String
+    var eEstudante: Boolean
+
+    idade = 19
+    altura = 1.80
+    nome = "Eduardo"
+    eEstudante = true
+
+    var idade2 = 20
+    var dobroAltura = altura * 2
+    var saudacao = "Olá, $nome!"
+
+    println("Idade: $idade")
+    println("Altura: $altura")
+    println("Nome: $nome")
+    println("É estudante? $eEstudante")
+    println("Soma das idades: $idade2")
+    println("Dobro das altura: $dobroAltura")
+    println(saudacao)
+    
+}
